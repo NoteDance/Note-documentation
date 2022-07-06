4ca239b4a2c6137854e3b74653ed54568aca23c1
7NoteDancing <63648431+7NoteDancing@users.noreply.github.com>
2022-07-06 17:06:22 +0800

    Update cnn.py

--- Note 3.0 documentation/DL/cnn.py
+++ Note 3.0 documentation/DL/cnn.py
@@ -7,7 +7,7 @@ class cnn:
           tf.keras.layers.Flatten(input_shape=(28, 28)),
           tf.keras.layers.Dense(128,activation='relu'),
           tf.keras.layers.Dropout(0.2),
-          tf.keras.layers.Dense(10,activation='softmax')
+          tf.keras.layers.Dense(10)
           ])
         self.param=self.model.weights
         self.opt=tf.keras.optimizers.Adam()
@@ -19,5 +19,4 @@ class cnn:
     
     
     def loss(self,output,labels):
-        loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
-        return loss(labels,output)
+        return tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=output,labels=labels))        
