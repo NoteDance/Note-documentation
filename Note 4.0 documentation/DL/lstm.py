b3db2992800297481a0f19b16d5208394cec2d2a
7NoteDancing <63648431+7NoteDancing@users.noreply.github.com>
2022-07-22 18:46:01 +0800

    Add files via upload

---
+++ Note 4.0 documentation/DL/lstm.py
@@ -0,0 +1,23 @@
+import tensorflow as tf
+
+#https://tensorflow.google.cn/text/tutorials/text_classification_rnn
+class lstm:
+    def __init__(self,encoder):
+        self.model=tf.keras.Sequential([
+        encoder,
+        tf.keras.layers.Embedding(len(encoder.get_vocabulary()), 64, mask_zero=True),
+        tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64,  return_sequences=True)),
+        tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(32)),
+        tf.keras.layers.Dense(64, activation='relu'),
+        tf.keras.layers.Dropout(0.5),
+        tf.keras.layers.Dense(1)
+        ])
+    
+    
+    def fp(self,data):
+        return self.model(data)
+    
+    
+    def loss(self,output,labels):
+        bce=tf.keras.losses.BinaryCrossentropy(from_logits=True)
+        return bce(output,labels)
