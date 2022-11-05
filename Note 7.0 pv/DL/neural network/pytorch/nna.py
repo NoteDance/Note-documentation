882fe2e61a8cfd6155785884b550ccc3e5a01fb9
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2022-11-06 00:22:26 +0800

    Update nna.py

--- Note 7.0 pv/DL/neural network/pytorch/nna.py
+++ Note 7.0 pv/DL/neural network/pytorch/nna.py
@@ -34,12 +34,12 @@ class nn:
     
     
     def fp(self,x):
-        pred=self.model(x)
+        pred=self.model(x.to(self.device))
         return pred
     
     
     def loss(self,output,labels):
-        loss=self.loss_fn(output,labels)
+        loss=self.loss_fn(output,labels.to(self.device))
         return loss
     
     
