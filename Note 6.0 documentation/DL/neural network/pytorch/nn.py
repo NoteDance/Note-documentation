17dc170650a2975e139c15c7bdba389275624b21
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2022-11-08 14:02:38 +0800

    Update nn.py

--- Note 6.0 documentation/DL/neural network/pytorch/nn.py
+++ Note 6.0 documentation/DL/neural network/pytorch/nn.py
@@ -15,7 +15,7 @@ class NeuralNetwork(nn.Module):
         )
     
     
-    def forward(self, x):
+    def forward(self,x):
         x=self.flatten(x)
         logits=self.linear_relu_stack(x)
         return logits
