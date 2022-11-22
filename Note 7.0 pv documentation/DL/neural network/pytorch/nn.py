80ce2f23c20ec8ddc532c096c07ada7ef5f5005e
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2022-11-22 14:27:21 +0800

    Update nn.py

--- Note 7.0 pv documentation/DL/neural network/pytorch/nn.py
+++ Note 7.0 pv documentation/DL/neural network/pytorch/nn.py
@@ -1,42 +1,31 @@
-import torch
-from torch import nn
+import tensorflow as tf
 
 
-class NeuralNetwork(nn.Module):
+class nn:               #a simple example,a neural network class
     def __init__(self):
-        super(NeuralNetwork,self).__init__()
-        self.flatten=nn.Flatten()
-        self.linear_relu_stack=nn.Sequential(
-            nn.Linear(28*28,512),
-            nn.ReLU(),
-            nn.Linear(512,512),
-            nn.ReLU(),
-            nn.Linear(512,10)
-        )
+        self.weight1=tf.Variable(tf.random.normal([784,64]))
+        self.bias1=tf.Variable(tf.random.normal([64]))
+        self.weight2=tf.Variable(tf.random.normal([64,64]))
+        self.bias2=tf.Variable(tf.random.normal([64]))
+        self.weight3=tf.Variable(tf.random.normal([64,10]))
+        self.bias3=tf.Variable(tf.random.normal([10]))
+        self.param=[self.weight1,self.weight2,self.weight3,self.bias1,self.bias2,self.bias3]
+        self.optimizer=tf.keras.optimizers.Adam()
+        self.info='example'
     
     
-    def forward(self,x):
-        x=self.flatten(x)
-        logits=self.linear_relu_stack(x)
-        return logits
-
-
-class nn:
-    def __init__(self,device):
-        if torch.cuda.is_available():
-            self.device=torch.device('cuda')
-        else:
-            self.device=torch.device('cpu')
-        self.model=NeuralNetwork().to(self.device)
-        self.loss_fn=nn.CrossEntropyLoss()
-        self.opt=torch.optim.SGD(self.model.parameters(),lr=1e-3)
-    
-    
-    def fp(self,x):
-        pred=self.model(x.to(self.device))
-        return pred
+    def fp(self,data):
+        with tf.device('GPU:0'):
+            layer1=tf.nn.relu(tf.matmul(data,self.param[0])+self.param[3])
+            layer2=tf.nn.relu(tf.matmul(layer1,self.param[1])+self.param[4])
+            output=tf.matmul(layer2,self.param[2])+self.param[5]
+        return output
     
     
     def loss(self,output,labels):
-        loss=self.loss_fn(output,labels.to(self.device))
-        return loss
+        return tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=output,labels=labels))
+    
+
+    def opt(self,gradient,param):
+        self.optimizer.apply_gradients(zip(gradient,param))
+        return
