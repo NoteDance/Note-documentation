0e35c2d2ea4b26a24f3defd87b1ef8af44b36a8a
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2022-11-22 14:31:35 +0800

    Update DuelingDQN.py

--- Note 7.0 pv documentation/RL/neural network/DuelingDQN.py
+++ Note 7.0 pv documentation/RL/neural network/DuelingDQN.py
@@ -55,9 +55,13 @@ class DuelingDQN:
         return F.mse_loss(q_value,target)
     
     
-    def opt(self,loss):
+    def backward(self,loss):
         self.optimizer.zero_grad()
         loss.backward()
+        return
+    
+    
+    def opt(self):
         self.optimizer.step()
         return
         
