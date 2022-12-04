21a98628826afc943c65e5b1b336db1cf51f88eb
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2022-12-04 13:28:12 +0800

    Update DuelingDQN.py

--- Note 6.0 documentation/RL/neural network/DuelingDQN.py
+++ Note 6.0 documentation/RL/neural network/DuelingDQN.py
@@ -21,11 +21,13 @@ class VAnet(torch.nn.Module):
 class DuelingDQN:
     def __init__(self,state_dim,hidden_dim,action_dim):
         if torch.cuda.is_available():
-            self.device=torch.device('cuda')
+            self.device_d=torch.device('cuda')
+            self.device_n=torch.device('cuda')
         else:
-            self.device=torch.device('cpu')
-        self.nn=VAnet(state_dim,hidden_dim,action_dim).to(self.device)
-        self.target_q_net=VAnet(state_dim,hidden_dim,action_dim).to(self.device)
+            self.device_d=torch.device('cpu')
+            self.device_n=torch.device('cpu')
+        self.nn=VAnet(state_dim,hidden_dim,action_dim).to(self.device_n)
+        self.target_q_net=VAnet(state_dim,hidden_dim,action_dim).to(self.device_n)
         self.optimizer=torch.optim.Adam(self.q_net.parameters(),lr=2e-3)
         self.genv=gym.make('CartPole-v0')
 
@@ -33,22 +35,19 @@ class DuelingDQN:
     
     def env(self,a=None,initial=None):
         if initial==True:
-            self.genv.action_space.seed(0)
-            state,info=self.genv.reset(seed=0,return_info=True)
+            state=self.genv.reset(seed=0)
             return state
         else:
             next_state,reward,done,_=self.genv.step(a)
-            if done:
-                next_state,info=self.genv.reset(return_info=True)
             return next_state,reward,done
     
     
     def loss(self,s,a,next_s,r,d):
-        s=torch.tensor(s,dtype=torch.float).to(self.device)
-        a=torch.tensor(a).view(-1,1).to(self.device)
-        next_s=torch.tensor(next_s,dtype=torch.float).to(self.device)
-        r=torch.tensor(r,dtype=torch.float).view(-1,1).to(self.device)
-        d=torch.tensor(d,dtype=torch.float).view(-1,1).to(self.device)
+        s=torch.tensor(s,dtype=torch.float).to(self.device_d)
+        a=torch.tensor(a).view(-1,1).to(self.device_d)
+        next_s=torch.tensor(next_s,dtype=torch.float).to(self.device_d)
+        r=torch.tensor(r,dtype=torch.float).view(-1,1).to(self.device_d)
+        d=torch.tensor(d,dtype=torch.float).view(-1,1).to(self.device_d)
         q_value=self.nn(s).gather(1,a)
         next_q_value=self.target_q_net(next_s).max(1)[0].view(-1,1)
         target=r+0.98*next_q_value*(1-d)
@@ -63,5 +62,5 @@ class DuelingDQN:
         
     
     def update_param(self):
-        self.target_q_net.load_state_dict(self.q_net.state_dict())
+        self.target_q_net.load_state_dict(self.nn.state_dict())
         return
