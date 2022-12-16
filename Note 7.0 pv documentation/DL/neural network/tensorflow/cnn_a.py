a32bda7625ee25904cb3ea08b65a10a4ba502bb5
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2022-12-16 13:34:48 +0800

    Update cnn_a.py

--- Note 7.0 pv documentation/DL/neural network/tensorflow/cnn_a.py
+++ Note 7.0 pv documentation/DL/neural network/tensorflow/cnn_a.py
@@ -18,8 +18,7 @@ class cnn:
     
     
     def fp(self,data):
-        with tf.device('GPU:0'):
-            output=self.model(data)
+        output=self.model(data)
         return output
     
     
