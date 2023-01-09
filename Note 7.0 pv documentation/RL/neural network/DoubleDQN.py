e4cd3502895e0f37c2f5b87178631e05469277b2
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-01-09 21:24:20 +0800

    Update DoubleDQN.py

--- Note 7.0 pv documentation/RL/neural network/DoubleDQN.py
+++ Note 7.0 pv documentation/RL/neural network/DoubleDQN.py
@@ -41,7 +41,7 @@ class DoubleDQN:
     
     def loss(self,s,a,next_s,r,d):
         s=torch.tensor(s,dtype=torch.float).to(self.device_d)
-        a=torch.tensor(a).view(-1,1).to(self.device_d)
+        a=torch.tensor(a,dtype=torch.int64).view(-1,1).to(self.device_d)
         next_s=torch.tensor(next_s,dtype=torch.float).to(self.device_d)
         r=torch.tensor(r,dtype=torch.float).view(-1,1).to(self.device_d)
         d=torch.tensor(d,dtype=torch.float).view(-1,1).to(self.device_d)
