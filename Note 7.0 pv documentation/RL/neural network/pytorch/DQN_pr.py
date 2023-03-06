30d57eeb2c6b0590fc203326d2e0dafd0213fea1
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-03-06 20:55:34 +0800

    Update DQN_pr.py

--- Note 7.0 pv documentation/RL/neural network/pytorch/DQN_pr.py
+++ Note 7.0 pv documentation/RL/neural network/pytorch/DQN_pr.py
@@ -19,13 +19,11 @@ class Qnet(torch.nn.Module):
 class DQN:
     def __init__(self,state_dim,hidden_dim,action_dim):
         if torch.cuda.is_available():
-            self.device_d=torch.device('cuda')
-            self.device_n=torch.device('cuda')
+            self.device=torch.device('cuda')
         else:
-            self.device_d=torch.device('cpu')
-            self.device_n=torch.device('cpu')
-        self.nn=Qnet(state_dim,hidden_dim,action_dim).to(self.device_n)
-        self.target_q_net=Qnet(state_dim,hidden_dim,action_dim).to(self.device_n)
+            self.device=torch.device('cpu')
+        self.nn=Qnet(state_dim,hidden_dim,action_dim).to(self.device)
+        self.target_q_net=Qnet(state_dim,hidden_dim,action_dim).to(self.device)
         self.pr=pr.pr()
         self.initial_TD=7
         self._epsilon=0.0007
@@ -49,11 +47,11 @@ class DQN:
         
     
     def loss(self,s,a,next_s,r,d):
-        s=torch.tensor(s,dtype=torch.float).to(self.device_d)
-        a=torch.tensor(a,dtype=torch.int64).view(-1,1).to(self.device_d)
-        next_s=torch.tensor(next_s,dtype=torch.float).to(self.device_d)
-        r=torch.tensor(r,dtype=torch.float).view(-1,1).to(self.device_d)
-        d=torch.tensor(d,dtype=torch.float).view(-1,1).to(self.device_d)
+        s=torch.tensor(s,dtype=torch.float).to(self.device)
+        a=torch.tensor(a,dtype=torch.int64).view(-1,1).to(self.device)
+        next_s=torch.tensor(next_s,dtype=torch.float).to(self.device)
+        r=torch.tensor(r,dtype=torch.float).view(-1,1).to(self.device)
+        d=torch.tensor(d,dtype=torch.float).view(-1,1).to(self.device)
         q_value=self.nn(s).gather(1,a)
         next_q_value=self.target_q_net(next_s).max(1)[0].view(-1,1)
         target=r+0.98*next_q_value*(1-d)
