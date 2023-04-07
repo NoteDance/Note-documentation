648980618af5ef9d0f7fa1dbf593302fbe989f41
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-04-07 20:13:33 +0800

    Add files via upload

---
+++ Note 7.0 pv documentation/DL/neural network/tensorflow/nn_layer.py
@@ -0,0 +1,26 @@
+import tensorflow as tf
+import layer.dense as d
+
+
+class nn:
+    def __init__(self):
+        self.loss_object=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+        self.train_accuracy=tf.keras.metrics.SparseCategoricalAccuracy(name='train_accuracy')
+        self.opt=tf.keras.optimizers.Adam()
+    
+    
+    def init(self):
+        self.layer1=d.dense([784,128],weight_func=['uniform',0,0.01],activation='relu')
+        self.layer2=d.dense([128,10],weight_func=['uniform',0,0.01])
+        self.param=[self.layer1.weight,self.layer1.bias,self.layer2.weight,self.layer2.bias]
+        return
+    
+    
+    def fp(self,data):
+        output1=self.layer1.output(data)
+        output2=self.layer2.output(output1)
+        return output2
+    
+    
+    def loss(self,output,labels):
+        return self.loss_object(labels,output)
