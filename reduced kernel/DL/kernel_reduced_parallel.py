7dd4a660d5fe3a604ff93eac4866018bd476b382
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-05-05 16:43:37 +0800

    Update kernel_reduced_parallel.py

--- reduced kernel/DL/kernel_reduced_parallel.py
+++ reduced kernel/DL/kernel_reduced_parallel.py
@@ -133,7 +133,7 @@ class kernel:
                             self.total_acc=np.zeros(self.process_thread,dtype=np.float32)
                 except AttributeError:
                     pass
-            try:  #If neural network object defining attenuate function,kernel will initialize opt_counter.
+            try:  #If neural network object define attenuate function,kernel will initialize opt_counter.
                 if self.nn.attenuate!=None:
                     if type(self.process_thread)==list:
                         self.opt_counter=np.zeros([self.process_thread[0]*self.process_thread[1]],dtype=np.float32)
@@ -198,7 +198,7 @@ class kernel:
     #Optimization subfunction,it be used for opt function,it used optimization function of tensorflow platform and parallel optimization.
     @function(jit_compile=True)
     def tf_opt_t(self,data,labels,t=None,ln=None,u=None):
-        try:  #If neural network object defining GradientTape function,kernel will use it or else use other.
+        try:  #If neural network object define GradientTape function,kernel will use it or else use other.
             if self.nn.GradientTape!=None:
                 if type(self.process_thread)==list:
                     tape,output,loss=self.nn.GradientTape(data,labels,u)
@@ -207,7 +207,7 @@ class kernel:
         except AttributeError:
             with self.platform.GradientTape(persistent=True) as tape:
                 try:
-                    try:  #If neural network object defining one argument value fp function,kernel will use it or else use other.
+                    try:  #If neural network object define one argument value fp function,kernel will use it or else use other.
                         output=self.nn.fp(data)
                         loss=self.nn.loss(output,labels)
                     except TypeError:
@@ -234,7 +234,7 @@ class kernel:
             pass
         if self.PO==1:
             self.lock[0].acquire()
-            try:  #If neural network object defining gradient function,kernel will use it or else use other.
+            try:  #If neural network object define gradient function,kernel will use it or else use other.
                 gradient=self.nn.gradient(tape,loss)
             except AttributeError:
                 gradient=tape.gradient(loss,self.nn.param)
@@ -261,7 +261,7 @@ class kernel:
             self.lock[0].release()
         elif self.PO==2:
             self.lock[0].acquire()
-            try:  #If neural network object defining gradient function,kernel will use it or else use other.
+            try:  #If neural network object define gradient function,kernel will use it or else use other.
                 gradient=self.nn.gradient(tape,loss)
             except AttributeError:
                 gradient=tape.gradient(loss,self.nn.param)
