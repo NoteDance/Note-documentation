a2b0b5791d870135424ec57c0f77babce6e69668
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-05-12 20:51:12 +0800

    Update nn_Momentum.py

--- Note 7.0 pv documentation/DL/neural network/tensorflow/nn_Momentum.py
+++ Note 7.0 pv documentation/DL/neural network/tensorflow/nn_Momentum.py
@@ -21,13 +21,14 @@ class Momentum: #You can also realize optimizer by yourself
 
 class nn:               #A neural network class example,use optimizer written by yourself.
     def __init__(self):
-        self.weight1=tf.Variable(tf.random.normal([784,64])) #You can also customize parameters.
-        self.bias1=tf.Variable(tf.random.normal([64]))
-        self.weight2=tf.Variable(tf.random.normal([64,64]))
-        self.bias2=tf.Variable(tf.random.normal([64]))
-        self.weight3=tf.Variable(tf.random.normal([64,10]))
-        self.bias3=tf.Variable(tf.random.normal([10]))
+        self.weight1=tf.Variable(tf.random.normal([784,64],dtype='float64')) #You can also customize parameters.
+        self.bias1=tf.Variable(tf.random.normal([64],dtype='float64'))
+        self.weight2=tf.Variable(tf.random.normal([64,64],dtype='float64'))
+        self.bias2=tf.Variable(tf.random.normal([64],dtype='float64'))
+        self.weight3=tf.Variable(tf.random.normal([64,10],dtype='float64'))
+        self.bias3=tf.Variable(tf.random.normal([10],dtype='float64'))
         self.param=[self.weight1,self.weight2,self.weight3,self.bias1,self.bias2,self.bias3] #parameter list,kernel uses it list for backpropagation.
+        self.loss_object=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
         self.optimizer=Momentum(0.07,0.7) #optimizer,kernel uses it to optimize.
         self.info='example'
     
@@ -40,7 +41,7 @@ class nn:               #A neural network class example,use optimizer written by
     
     
     def loss(self,output,labels): #loss functino,kernel uses it to calculate loss.
-        return tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=output,labels=labels))
+        return self.loss_object(labels,output)
     
 
     def opt(self,gradient,param): #optimization function,kernel uses it to optimize.
