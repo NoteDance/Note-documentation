64375405efc6e81a6361341cb90e0bceea67d1c8
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-05-16 18:32:11 +0800

    Update nn_.py

--- Note 7.0 pv documentation/DL/neural network/tensorflow/process/nn_.py
+++ Note 7.0 pv documentation/DL/neural network/tensorflow/process/nn_.py
@@ -15,15 +15,10 @@ class nn:
         self.optimizer=o.Momentum(0.07,0.7)
     
     
-    def fp(self,data,param=None):
-        if param==None:
-            layer1=tf.nn.relu(tf.matmul(data,self.weight1)+self.bias1)
-            layer2=tf.nn.relu(tf.matmul(layer1,self.weight2)+self.bias2)
-            output=tf.matmul(layer2,self.weight3)+self.bias3
-        else:
-            layer1=tf.nn.relu(tf.matmul(data,param[0])+param[3])
-            layer2=tf.nn.relu(tf.matmul(layer1,param[1])+param[4])
-            output=tf.matmul(layer2,param[2])+param[5]
+    def fp(self,data):
+        layer1=tf.nn.relu(tf.matmul(data,self.weight1)+self.bias1)
+        layer2=tf.nn.relu(tf.matmul(layer1,self.weight2)+self.bias2)
+        output=tf.matmul(layer2,self.weight3)+self.bias3
         return output
     
     
