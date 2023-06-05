9a0403ec494c03a248c022c83c4a554ac7726906
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-06-05 23:54:09 +0800

    Update cnn.py

--- Note 7.0 pv documentation/DL/neural network/tensorflow/process/cnn.py
+++ Note 7.0 pv documentation/DL/neural network/tensorflow/process/cnn.py
@@ -11,7 +11,7 @@ class cnn:
         self.loss_object=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
         self.optimizer=o.Adam()
         # Initialize a variable to keep track of the batch count
-        self.bc=tf.Variable(0,dtype=tf.float64)
+        self.bc=tf.Variable(0,dtype=tf.float32)
     
     
     def build(self):
