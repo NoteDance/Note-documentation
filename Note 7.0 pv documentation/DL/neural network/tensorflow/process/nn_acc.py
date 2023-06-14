5be519976eb97422db421604ff98e4bebae0e98c
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-06-15 00:28:12 +0800

    Add files via upload

---
+++ Note 7.0 pv documentation/DL/neural network/tensorflow/process/nn_acc.py
@@ -0,0 +1,44 @@
+import tensorflow as tf
+import Note.nn.layer.dense as d
+import Note.nn.process.optimizer as o
+from Note.nn.layer.flatten import flatten
+
+# Define a neural network class
+class nn:
+    def __init__(self):
+        # Initialize the loss function and the optimizer
+        self.loss_object=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+        self.train_accuracy=tf.keras.metrics.SparseCategoricalAccuracy(name='train_accuracy')
+        self.optimizer=o.Momentum(0.07,0.7)
+    
+    
+    def build(self):
+        # Create two dense layers with relu and linear activations
+        self.layer1=d.dense([784,128],activation='relu')
+        self.layer2=d.dense([128,10])
+        # Store the parameters of the layers in a list
+        self.param=[self.layer1.param,self.layer2.param]
+        return
+    
+    
+    def fp(self,data):
+        # Perform forward propagation on the input data
+        data=flatten(data)
+        output1=self.layer1.output(data)
+        output2=self.layer2.output(output1)
+        return output2
+    
+    
+    def loss(self,output,labels):
+        # Compute the loss between the output and the labels
+        return self.loss_object(labels,output)
+    
+    
+    def accuracy(self,output,labels): #accuracy function,kernel uses it to calculate accuracy.
+        return self.train_accuracy(labels,output)
+    
+
+    def opt(self,gradient):
+        # Perform optimization on the parameters using the gradient
+        param=self.optimizer.opt(gradient,self.param)
+        return param
