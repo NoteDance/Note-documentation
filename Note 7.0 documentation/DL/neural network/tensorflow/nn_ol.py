c4ad2d0962318ed195ab354f3056ef83c975e4c4
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-07-12 13:59:23 +0800

    Update nn_ol.py

--- Note 7.0 documentation/DL/neural network/tensorflow/nn_ol.py
+++ Note 7.0 documentation/DL/neural network/tensorflow/nn_ol.py
@@ -16,13 +16,13 @@ class nn:
         self.loss_object=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
         self.opt=tf.keras.optimizers.Adam() #optimizer,kernel uses it to optimize.
         self.train_loss_list=[]
-        self.c=0 #counter
+        self.counter=0 #counter
         self.max_length=1000
     
     
-    def ol(self): #This is simulative online function.
+    def online(self): #This is simulative online function.
         index=np.random.choice(60000,size=[32])
-        if self.c==10000:
+        if self.counter==10000:
             return 'stop'
         else:
             return [self.train_data[index],self.train_labels[index]]
