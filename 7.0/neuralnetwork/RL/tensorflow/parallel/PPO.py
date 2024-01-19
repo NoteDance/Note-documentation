418e64c1d2614e6281c7478ef2ea0613b107530a
NoteDance <63648431+NoteDance@users.noreply.github.com>
2024-01-19 21:44:30 +0800

    Add files via upload

---
+++ 7.0/neuralnetwork/RL/tensorflow/parallel/PPO.py
@@ -0,0 +1,76 @@
+import tensorflow as tf
+import gym
+from Note.nn.parallel.optimizer import SGD
+
+
+class actor:
+    def __init__(self,state_dim,hidden_dim,action_dim):
+        self.weight1=tf.Variable(tf.random.normal([state_dim,hidden_dim]))
+        self.bias1=tf.Variable(tf.random.normal([hidden_dim]))
+        self.weight2=tf.Variable(tf.random.normal([hidden_dim,action_dim]))
+        self.bias2=tf.Variable(tf.random.normal([action_dim]))
+        self.param=[self.weight1,self.bias1,self.weight2,self.bias2]
+    
+    def fp(self,x):
+        x=tf.nn.relu(tf.matmul(x,self.weight1)+self.bias1)
+        return tf.nn.softmax(tf.matmul(x,self.weight2)+self.bias2)
+
+
+class critic:
+    def __init__(self,state_dim,hidden_dim):
+        self.weight1=tf.Variable(tf.random.normal([state_dim,hidden_dim]))
+        self.bias1=tf.Variable(tf.random.normal([hidden_dim]))
+        self.weight2=tf.Variable(tf.random.normal([hidden_dim,1]))
+        self.bias2=tf.Variable(tf.random.normal([1]))
+        self.param=[self.weight1,self.bias1,self.weight2,self.bias2]
+    
+    def fp(self,x):
+        x=tf.nn.relu(tf.matmul(x,self.weight1)+self.bias1)
+        return tf.matmul(x,self.weight2)+self.bias2
+    
+    
+class PPO:
+    def __init__(self,state_dim,hidden_dim,action_dim,clip_eps):
+        self.nn=actor(state_dim,hidden_dim,action_dim)
+        self.critic=critic(state_dim,hidden_dim)
+        self.actor_old=actor(state_dim,hidden_dim,action_dim)
+        self.clip_eps=clip_eps
+        self.param=[self.actor.param,self.critic.param]
+        self.opt=SGD(param=self.param)
+        self.genv=[gym.make('CartPole-v0') for _ in range(5)]
+    
+    
+    def env(self,a=None,initial=None):
+        if initial==True:
+            state=self.genv.reset(seed=0)
+            return state
+        else:
+            next_state,reward,done,_=self.genv.step(a)
+            return next_state,reward,done
+    
+    
+    def loss(self,s,a,next_s,r,d):
+        a=tf.expand_dims(a,axis=1)
+        raito=tf.gather(self.actor.fp(s),a,axis=1,batch_dims=1)/tf.gather(self.actor_old.fp(s),a,axis=1,batch_dims=1)
+        value=tf.gather(self.critic.fp(s),a,axis=1,batch_dims=1)
+        value_tar=r+tf.gather(self.critic.fp(next_s),a,axis=1,batch_dims=1)
+        TD=r+0.98*value_tar*(1-d)-value
+        clip=tf.clip_by_value(raito,clip_value_min=1-self.clip_eps,clip_value_max=1+self.clip_eps)*TD
+        return [tf.reduce_mean(clip),tf.reduce_mean((TD)**2)]
+    
+    
+    def gradient(self,tape,loss):
+        actor_gradient=tape.gradient(loss[0],self.param[0])
+        critic_gradient=tape.gradient(loss[1],self.param[1])
+        return [actor_gradient,critic_gradient]
+    
+    
+    def opt(self,gradient):
+        self.opt.opt(zip(gradient[0],self.param[0]))
+        self.opt.opt(zip(gradient[1],self.param[1]))
+        return
+    
+    
+    def update_param(self):
+        self.actor_old.param=self.actor.param.copy()
+        return
