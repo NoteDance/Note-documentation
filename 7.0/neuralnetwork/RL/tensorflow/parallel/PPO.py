edf62e3efa6f8ac1f254f64baf9aaf25e35106b1
NoteDance <63648431+NoteDance@users.noreply.github.com>
2024-01-19 21:50:19 +0800

    Update PPO.py

--- 7.0/neuralnetwork/RL/tensorflow/parallel/PPO.py
+++ 7.0/neuralnetwork/RL/tensorflow/parallel/PPO.py
@@ -66,11 +66,7 @@ class PPO:
     
     
     def opt(self,gradient):
-        self.opt.opt(zip(gradient[0],self.param[0]))
-        self.opt.opt(zip(gradient[1],self.param[1]))
-        return
-    
-    
-    def update_param(self):
+        self.opt.opt(gradient[0],self.param[0])
+        self.opt.opt(gradient[1],self.param[1])
         self.actor_old.param=self.actor.param.copy()
         return
