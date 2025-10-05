45d1a4f6f9da92b9a243b84aafa3afc8555942a9
NoteDance <notedance@outlook.com>
2025-10-05 15:25:19 +0800

    Update DQN_pr.py

--- 7.0/models/RL/tensorflow/non_parallel/DQN_pr.py
+++ 7.0/models/RL/tensorflow/non_parallel/DQN_pr.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 from Note import nn
+import numpy as np
 import gym
 import Note.RL.rl.prioritized_replay as pr
 
@@ -21,7 +22,8 @@ class DQN:
         self.target_q_net=Qnet(state_dim,hidden_dim,action_dim)
         self.param=self.nn.param
         self.pr=pr.pr()
-        self.initial_TD=tf.Variable(7.)
+        self.initial_TD=np.array(7.).astype('float32')
+        self.pr.TD=self.initial_TD
         self.epsilon=0.0007
         self.alpha=0.7
         self.opt=tf.keras.optimizers.Adam()
