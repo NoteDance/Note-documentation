35367a3e3e37d500f7dc1425551c9f4a00520494
NoteDance <notedance@outlook.com>
2025-10-05 15:30:36 +0800

    Update DQN_pr.py

--- 7.0/models/RL/pytorch/parallel/DQN_pr.py
+++ 7.0/models/RL/pytorch/parallel/DQN_pr.py
@@ -27,8 +27,7 @@ class DQN:
         self.nn=Qnet(state_dim,hidden_dim,action_dim).to(self.device)
         self.target_q_net=Qnet(state_dim,hidden_dim,action_dim).to(self.device)
         self.pr=pr.pr_mp_()
-        self.initial_TD=np.array(7.).astype('float32')
-        self.pr.TD=self.initial_TD
+        self.pr.TD=np.array(7.).astype('float32')
         self.epsilon=0.0007
         self.alpha=0.7
         self.optimizer=[torch.optim.Adam(self.nn.parameters(),lr=2e-3) for _ in range(5)] #optimizer,kernel uses it to optimize.
